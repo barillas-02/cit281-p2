66bbc326f67753d7e9738fbf74022a85bd3c921e
barillas-02 <barillas@uoregon.edu>
2025-06-08 22:44:05 -0700

    Create p2-random.js

---
+++ p2-random.js
@@ -0,0 +1,50 @@
+/*
+    CIT 281 Project 2
+    Name: Isabella Barillas
+*/
+
+// Returns a random number between min (inclusive) and max (exclusive)
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  let result = "";
+  
+  for (let i = 0; i < getRandomInteger(5, 26); i++) {
+    result += alphabet[getRandomInteger(1, alphabet.length - 1)];
+  }
+  
+  console.log(result);
+
+
+  //Get and return a single, random, lowercase letter
+  function getRandomLetter() {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const randomIndex = Math.floor(Math.random() * alphabet.length);
+    return alphabet[randomIndex];
+}
+
+for (let i = 0; i < 1; i++) {
+      console.log(getRandomLetter());
+}
+
+
+//return the random length string
+function getRandomString(minLength, maxLength) {
+    const length = getRandomInteger(minLength, maxLength);
+    let result = "";
+
+    for (let i = 0; i < length; i++) {
+        result += getRandomLetter();
+    }
+
+    return result;
+}
+
+console.log(getRandomString(10, 20));
+
+
+//Returns a string in ascending order
+function getSortedString(string) { return string.split('').sort().join(''); }
+console.log(getSortedString(getRandomString(10, 20)));
